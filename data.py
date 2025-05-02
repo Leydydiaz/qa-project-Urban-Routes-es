45b16638c1d9a52466ffbc224c59fa04a5c574c0
Leydy Diaz <lmdiazm1@gmail.com>
2025-05-02 03:26:53 -0500

    data.py

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 # URL base del servicio Urban Routes
 urban_routes_url = "https://cnt-f1d3e48f-6ea2-415d-9cb7-b9d7dd28d1b3.containerhub.tripleten-services.com/?lng=es"
-
 # Datos del viaje
 address_from = "East 2nd Street, 601"
 address_to = "1300 1st St"
